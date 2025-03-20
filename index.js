47f65a72e29fc015825cfafce25ae6da4cf030d1
Douglas Bastos <dautodouglasdauto@gmail.com>
2025-03-20 17:37:08 -0300

    Adicionado aprimoramento e finalizado projeto

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 let computerNumber;
 let userNumbers = [];
+let attempts = 0;
+let maxGuesses = 10;
 
 function init() {
     computerNumber = Math.floor(Math.random() * 100 + 1);
@@ -11,11 +13,32 @@ function compareNumbers() {
     userNumbers.push(' ' + userNumber);
     document.getElementById("guesses").innerHTML = userNumbers;
 
-    if (userNumber > computerNumber) {
-        document.getElementById("textOutput").innerHTML = "<p>Your number is hight.</p>";
-    } else if (userNumber < computerNumber) {
-        document.getElementById("textOutput").innerHTML = "<p>Your number is low.</p>";
+    if (attempts < maxGuesses) {
+        if (userNumber > computerNumber) {
+            insertValues("<p>Your number is hight.</p>");
+        } else if (userNumber < computerNumber) {
+            insertValues("<p>Your number is low.</p>");
+        } else {
+            insertValues("<p>You got it right! :D</p>");
+        }
     } else {
-        document.getElementById("textOutput").innerHTML = "<p>You got it right!</p>";
+        insertValues("<p>You lose! :c</p>");
     }
+
+    document.getElementById("inputBox").value = '';
+    document.getElementById("inputBox").focus();
+
+    function insertValues(text) {
+        document.getElementById("textOutput").innerHTML = text;
+        document.getElementById("attempts").innerHTML = ++attempts;
+
+        if (text === "<p>You lose! :c</p>" || text === "<p>You got it right! :D</p>") {
+            document.getElementById("inputBox").setAttribute('Readonly', 'Readonly');
+            document.getElementById("compare").setAttribute('onclick', '');
+        }
+    }
+}
+
+function newGame() {
+    window.location.reload();
 }
